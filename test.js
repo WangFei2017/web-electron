c97fe8e2d6394672ba8584f20cd311e98980e25f
‘'wangFei <wangfei@afcatstar.com.cn>
2017-10-27 16:20:11 +0800

    第三次提交

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("argv: ",process.argv);
 process.on('uncaughtException', function (err) {
-   console.error('An uncaught error occurred!');
+   console.error('111111111111111111');
    console.error(err.stack);
  });
